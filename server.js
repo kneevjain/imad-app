5901e15eb7eef6c0212fab58c36a6ade310c2d6a
kneevjain <30682853+kneevjain@users.noreply.github.com>
2017-08-06 19:38:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/A1', function (req, res) {
 
 app.get('/A2', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'a2.html'));
-    
+});
     app.get('/app', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'app.html'));
     
